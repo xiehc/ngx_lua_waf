e5d2b7f17141cfc455fab26053f1b0ee1ca48dbd
xiehc <dekar_x@126.com>
2021-09-16 11:24:58 +0800

    Update handleWafLogIP.sh

--- handleWafLogIP.sh
+++ handleWafLogIP.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat /opt/ourpalm/logs/waf/*|awk '{print $1}'|perl -ne 'if($_ =~ /(^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$)/ ) {print $_}'|sort  |uniq -c |sort -n|perl -ne 'if($_ =~ /\s+(\d+)\s+(.*)/ ) {if ($1>1) {print "$2\n"}}' > /etc/nginx/waf/ngx_lua_waf/wafconf/blackip
+cat /opt/ourpalm/logs/waf/*|awk '{print $1}'|perl -ne 'if($_ =~ /(^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$)/ ) {print $_}'|sort  |uniq -c |sort -n|perl -ne 'if($_ =~ /\s+(\d+)\s+(.*)/ ) {if ($1>1) {print "$2\n"}}' > /etc/nginx/waf/ngx_lua_waf/wafconf/banip
